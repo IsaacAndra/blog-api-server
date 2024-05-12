9bd7b2dd2595936344a29e5141d993743dd0f642
Isaac Andrade <isaac.andra84@gmail.com>
2024-05-12 18:35:17 -0300

    feat: UserService Created

--- blog/src/main/java/com/isaacandrade/blog/service/UserService.java
+++ blog/src/main/java/com/isaacandrade/blog/service/UserService.java
@@ -1,2 +1,50 @@
-package com.isaacandrade.blog.service;public class UserService {
+package com.isaacandrade.blog.service;
+
+import com.isaacandrade.blog.domain.user.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public List<UserDTO> findAllUsers(){
+        List<User> user = userRepository.findAll();
+
+        return user.stream().map(this::mapToUserDTO).collect(Collectors.toList());
+    }
+
+    public UserDTO findUserById(Long id){
+        User userById = userRepository.findById(id).orElseThrow();
+
+        return mapToUserDTO(userById);
+    }
+
+    public UserDTO createUser(CreateUserDTO data){
+        User user = new User(data);
+        User savedUser = userRepository.save(user);
+
+        return mapToUserDTO(savedUser);
+    }
+
+    public UserDTO updateUser(Long id, EditUserDTO data){
+        User user = userRepository.findById(id).orElseThrow();
+        user.updateUser(data);
+        return mapToUserDTO(user);
+    }
+    
+    public void deleteUser(Long id){
+        User user = userRepository.findById(id).orElseThrow();
+        userRepository.delete(user);
+    }
+
+    private UserDTO mapToUserDTO(User user) {
+        return new UserDTO(user.getId(), user.getUserName(), user.getEmail(), user.getPassWord());
+    }
+
 }
