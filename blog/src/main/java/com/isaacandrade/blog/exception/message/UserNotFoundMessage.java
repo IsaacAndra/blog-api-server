2f13035d0a67c0a281164bc93919066d7d787210
Isaac Andrade <isaac.andra84@gmail.com>
2024-05-14 13:37:11 -0300

    feat: Including Custom UserExceptions

---
+++ blog/src/main/java/com/isaacandrade/blog/exception/message/UserNotFoundMessage.java
@@ -0,0 +1,13 @@
+package com.isaacandrade.blog.exception.message;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class UserNotFoundMessage {
+    private int status;
+    private String message;
+}
--- blog/src/main/java/com/isaacandrade/blog/service/UserService.java
+++ blog/src/main/java/com/isaacandrade/blog/service/UserService.java
@@ -1,6 +1,8 @@
 package com.isaacandrade.blog.service;
 
 import com.isaacandrade.blog.domain.user.*;
+import com.isaacandrade.blog.exception.ConstraintViolationException;
+import com.isaacandrade.blog.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,25 +17,38 @@ public class UserService {
 
     public List<UserDTO> findAllUsers(){
         List<User> user = userRepository.findAll();
+        if (user.isEmpty()){
+            throw new UserNotFoundException();
+        }
 
         return user.stream().map(this::mapToUserDTO).collect(Collectors.toList());
     }
 
     public UserDTO findUserById(Long id){
-        User userById = userRepository.findById(id).orElseThrow();
+        User userById = userRepository
+                .findById(id)
+                .orElseThrow(
+                        () -> new UserNotFoundException("User With Id "  + id + " Was Not Found!")
+                );
 
         return mapToUserDTO(userById);
     }
 
     public UserDTO createUser(CreateUserDTO data){
         User user = new User(data);
+        if (data.userName() == null || data.email() == null || data.passWord() == null){
+            throw new ConstraintViolationException("username, email and password cannot be null!");
+        }
         User savedUser = userRepository.save(user);
 
         return mapToUserDTO(savedUser);
     }
 
     public UserDTO updateUser(Long id, EditUserDTO data){
-        User user = userRepository.findById(id).orElseThrow();
+        User user = userRepository
+                .findById(id)
+                .orElseThrow(
+                        () -> new UserNotFoundException("User With Id "  + id + " Was Not Found!"));
         user.updateUser(data);
         userRepository.save(user);
 
@@ -41,7 +56,10 @@ public class UserService {
     }
 
     public void deleteUser(Long id){
-        User user = userRepository.findById(id).orElseThrow();
+        User user = userRepository
+                .findById(id)
+                .orElseThrow(
+                        () -> new UserNotFoundException("User With Id "  + id + " Was Not Found!"));
         userRepository.delete(user);
     }
 
