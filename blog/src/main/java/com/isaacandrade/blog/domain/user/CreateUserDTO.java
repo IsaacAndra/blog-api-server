50d5e80a90debf4d936aad1cfd11221637863d1a
IsaacAndra <isaac.andra84@gmail.com>
2024-08-26 16:53:05 -0300

    :sparkles: feat: Custom Json Serialization

--- blog/src/main/java/com/isaacandrade/blog/domain/user/CreateUserDTO.java
+++ blog/src/main/java/com/isaacandrade/blog/domain/user/CreateUserDTO.java
@@ -1,8 +1,13 @@
 package com.isaacandrade.blog.domain.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record CreateUserDTO(
+        @JsonProperty("name")
         String userName,
+        @JsonProperty("email")
         String email,
+        @JsonProperty("senha")
         String passWord
 ) {
 }
