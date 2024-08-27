21a373288a5e0c911db0465d8e0af7af3c87be89
IsaacAndra <isaac.andra84@gmail.com>
2024-08-27 12:48:14 -0300

    :sparkles: feat: content negotiation on post controller

--- blog/src/main/java/com/isaacandrade/blog/controller/PostController.java
+++ blog/src/main/java/com/isaacandrade/blog/controller/PostController.java
@@ -5,6 +5,7 @@ import com.isaacandrade.blog.domain.post.CreatePostDTO;
 import com.isaacandrade.blog.domain.post.EditPostDTO;
 import com.isaacandrade.blog.domain.post.PostDTO;
 import com.isaacandrade.blog.service.PostService;
+import com.isaacandrade.blog.utils.MediaType;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,32 +20,41 @@ public class PostController {
     @Autowired
     PostService postService;
 
-    @GetMapping
+    @GetMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
     public ResponseEntity<List<PostDTO>> findAllPosts(){
         List<PostDTO> posts = postService.findAllActives();
         return ResponseEntity.ok(posts);
     }
 
-    @GetMapping("/author/{authorId}")
+    @GetMapping(
+            value = "/author/{authorId}",
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
     public ResponseEntity<AuthorWithPostsDTO> getPostsByAuthor(@PathVariable Long authorId){
         AuthorWithPostsDTO postsByAuthor = postService.findPostsWithAuthorId(authorId);
         return ResponseEntity.ok(postsByAuthor);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping(
+            value = "/{id}",
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
     public ResponseEntity<PostDTO> findPostById(@PathVariable Long id){
         PostDTO postsById = postService.findById(id);
         return ResponseEntity.ok(postsById);
     }
 
-    @PostMapping
+    @PostMapping(
+            consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML},
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
     @Transactional
     public ResponseEntity<PostDTO> createPost(@RequestBody CreatePostDTO data, @RequestParam Long authorId){
         PostDTO creatingPost = postService.createPost(data, authorId);
         return new ResponseEntity<>(creatingPost, HttpStatus.CREATED);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping(
+            value = "/{id}",
+            consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML},
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
     @Transactional
     public ResponseEntity<PostDTO> updatePost(@PathVariable Long id, @RequestBody EditPostDTO data){
         PostDTO editedPost = postService.updatePost(id, data);
