513f5a3401a6bc0cb5e08150696b5c250d183363
IsaacAndra <isaac.andra84@gmail.com>
2024-08-27 14:35:56 -0300

    :broom: fix: limpando a sujeira

--- blog/src/main/java/com/isaacandrade/blog/domain/post/PostDTO.java
+++ blog/src/main/java/com/isaacandrade/blog/domain/post/PostDTO.java
@@ -2,10 +2,8 @@ package com.isaacandrade.blog.domain.post;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.isaacandrade.blog.domain.user.UserDTO;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 
 
 @JsonPropertyOrder({"id", "title", "content", "createdAt", "isActive"})
