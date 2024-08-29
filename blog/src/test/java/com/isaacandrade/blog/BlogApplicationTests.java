fb0350a3d9c860e71ce6b35bc746be05893b579d
IsaacAndra <isaac.andra84@gmail.com>
2024-08-28 23:01:27 -0300

    :broom: cleanup: Removendo teste desnecessário

--- blog/src/test/java/com/isaacandrade/blog/BlogApplicationTests.java
+++ blog/src/test/java/com/isaacandrade/blog/BlogApplicationTests.java
@@ -1,30 +1,16 @@
 package com.isaacandrade.blog;
 
-import com.isaacandrade.blog.domain.user.UserRepository;
-import com.isaacandrade.blog.service.UserService;
+import com.isaacandrade.blog.integrationtests.config.MyIntegrationTest;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class BlogApplicationTests {
-	@Autowired
-	private UserService userService;
-
-	@Autowired
-	private UserRepository userRepository;
 
 	@Test
-	void contextLoads() {
-		// Verificando se o contexto carrega o UserService
-		assertNotNull(userService, "UserService deve ser carregado no contexto");
-
-		// Verificando se o contexto carrega o UserRepository
-		assertNotNull(userRepository, "UserRepository deve ser carregado no contexto");
-	}
+	void contextLoads() {}
 
 }
