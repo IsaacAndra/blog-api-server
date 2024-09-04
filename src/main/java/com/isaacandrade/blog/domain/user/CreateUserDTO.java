eaee1ced053c2e128ef51e98a9b15a6a4b9b0c06
IsaacAndra <isaac.andra84@gmail.com>
2024-09-03 22:51:39 -0300

    feat: Implementando Roles ao DTO

--- src/main/java/com/isaacandrade/blog/domain/user/CreateUserDTO.java
+++ src/main/java/com/isaacandrade/blog/domain/user/CreateUserDTO.java
@@ -1,13 +1,17 @@
 package com.isaacandrade.blog.domain.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
 
 public record CreateUserDTO(
         @JsonProperty("username")
         String userName,
         @JsonProperty("email")
+        @Email
         String email,
         @JsonProperty("senha")
-        String passWord
+        String passWord,
+
+        UserRole role
 ) {
 }
