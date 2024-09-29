35486653cc350b15443bb5667545b6e468a05cd1
IsaacAndra <isaac.andra84@gmail.com>
2024-09-29 19:51:41 -0300

    feat: Criando exceptions customizadas

--- src/main/java/com/isaacandrade/blog/exception/AboutNotFoundException.java
+++ src/main/java/com/isaacandrade/blog/exception/AboutNotFoundException.java
@@ -1,4 +1,6 @@
 package com.isaacandrade.blog.exception;
 
-public class AboutNotFoundException {
+public class AboutNotFoundException extends Throwable {
+    public AboutNotFoundException () {super("About Not Found Exception");}
+    public AboutNotFoundException (String message) {super(message);}
 }
